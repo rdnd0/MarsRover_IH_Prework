df8f9cf739d787120c98ec96c92b7020caac6f00
David Redondo <rdnd201@gmail.com>
2019-01-01 15:23:32 +0100

    add more rovers with crashing validation

--- app.js
+++ app.js
@@ -4,36 +4,36 @@ var rover1 = {
   direction: "E", //N, S, W, E
   x: 0,
   y: 0,
-  travelLog:[],
+  travelLog:["["+rover1.x+" , "+rover1.y+"]"],
   // turn: 0,
-  name: "rover1"
+  name: "Rover1"
   };
 
 var rover2 = {
   direction: "W", //N, S, W, E
   x: 0,
   y: 5,
-  travelLog:[],
+  travelLog:["["+rover2.x+" , "+rover2.y+"]"],
   // turn: 0,
-  name: "rover2"
+  name: "Rover2"
   };
 
 var rover3 = {
     direction: "S", //N, S, W, E
     x: 5,
     y: 5,
-    travelLog:[],
+    travelLog:["["+rover3.x+" , "+rover3.y+"]"],
     // turn: 0,
-    name: "rover3"
+    name: "Rover3"
     };
 
 var rover4 = {
     direction: "N", //N, S, W, E
-    x: 10,
+    x: 9,
     y: 5,
-    travelLog:[],
+    travelLog:["["+rover4.x+" , "+rover4.y+"]"],
     // turn: 0,
-    name: "rover4"
+    name: "Rover4"
     };
 
 var rovers = [rover1, rover2, rover3, rover4];
@@ -91,157 +91,201 @@ function turnRight(rover){
 
 
 function moveForward(rover){
-  var notCurrentRover = rover;
-  if(rover === rover1){notCurrentRover = rover2;} else {notCurrentRover = rover1;}
+  var crashN = 0;
+  var crashS = 0;
+  var crashW = 0;
+  var crashE = 0;
   switch (rover.direction) {
     case "N":
       if (rover.x === 0) {
-        console.log("You are about to get out of the grid with " +rover.name+ " please provide a new instruction.");
+        console.log("You are about to get out of the grid with " +rover.name+ ", please provide a new instruction.");
         break;
       }
       else if(grid[rover.x-1][rover.y] === "X"){ 
-        console.log("There is an obstacle for " +rover.name+ "  can we do something else please?")
+        console.log("There is an obstacle for " +rover.name+ ", can we do something else please?");
         break;
       }
-      else if(grid[rover.x-1][rover.y] === grid[notCurrentRover.x][notCurrentRover.y]){ 
-        console.log("You are about to crash into " +notCurrentRover.name+ " can we do something else please?")
-        break;
+      else if(rover.x !== 0){
+        for (h=0; h<rovers.length; h++){ 
+          if(grid[rover.x-1][rover.y] === grid[rovers[h].x][rovers[h].y]){ 
+          console.log("You are about to crash into " +rovers[h].name+ ", can we do something else please?")
+          crashN = 1
+          ;break
+          } 
+        }
       }
-      else{ 
-      rover.x -= 1;
-      break;}
+        if (crashN === 0) {
+          rover.x -= 1;
+          } break;  
 
     case "S":
       if (rover.x === 9) {
-      console.log("You are about to get out of the grid with " +rover.name+ " please provide a new instruction.");
+      console.log("You are about to get out of the grid with " +rover.name+ ", please provide a new instruction.");
       break;}
       else if(grid[rover.x+1][rover.y] === "X"){ 
-        console.log("There is an obstacle for " +rover.name+ " can we do something else please?")
+        console.log("There is an obstacle for " +rover.name+ ", can we do something else please?");
         break;
       }
-      else if(grid[rover.x+1][rover.y] === grid[notCurrentRover.x][notCurrentRover.y]){ 
-        console.log("You are about to crash into " +notCurrentRover.name+ " can we do something else please?")
-        break;
+      else if(rover.x !== 0){
+        for (h=0; h<rovers.length; h++){ 
+          if(grid[rover.x+1][rover.y] === grid[rovers[h].x][rovers[h].y]){ 
+          console.log("You are about to crash into " +rovers[h].name+ ", can we do something else please?")
+          crashS = 1
+          ;break
+          } 
+        }
       }
-      else{ 
-      rover.x += 1;
-      break;}
+        if (crashS === 0) {
+          rover.x += 1;
+          } break;  
 
     case "W":
       if (rover.y === 0) {
-        console.log("You are about to get out of the grid with " +rover.name+ " please provide a new instruction.");
+        console.log("You are about to get out of the grid with " +rover.name+ ", please provide a new instruction.");
         break;
       }
       else if(grid[rover.x][rover.y-1] === "X"){ 
-        console.log("There is an obstacle for " +rover.name+ " can we do something else please?")
+        console.log("There is an obstacle for " +rover.name+ ", can we do something else please?");
         break;
       }
-      else if(grid[rover.x][rover.y-1] === grid[notCurrentRover.x][notCurrentRover.y]){ 
-        console.log("You are about to crash into " +notCurrentRover.name+ " can we do something else please?")
-        break;
+      else if(rover.y !== 0){
+        for (h=0; h<rovers.length; h++){ 
+          if(grid[rover.x][rover.y-1] === grid[rovers[h].x][rovers[h].y]){ 
+          console.log("You are about to crash into " +rovers[h].name+ ", can we do something else please?")
+          crashW = 1
+          ;break
+          } 
+        }
       }
-      else{ 
-      rover.y -= 1;
-      break;}
+        if (crashW === 0) {
+          rover.y -= 1;
+          } break; 
 
     case "E":
       if (rover.y === 9) {
-        console.log("You are about to get out of the grid with " +rover.name+ " please provide a new instruction.");
+        console.log("You are about to get out of the grid with " +rover.name+ " ,please provide a new instruction.");
         break;
       }
       else if(grid[rover.x][rover.y+1] === "X"){ 
-        console.log("There is an obstacle for " +rover.name+ " can we do something else please?")
+        console.log("There is an obstacle for " +rover.name+ " ,can we do something else please?");
         break;
       }
-      else if(grid[rover.x][rover.y+1] === grid[notCurrentRover.x][notCurrentRover.y]){ 
-        console.log("You are about to crash into " +notCurrentRover.name+ " can we do something else please?")
-        break;
+      else if(rover.y !== 0){
+        for (h=0; h<rovers.length; h++){ 
+          if(grid[rover.x][rover.y+1] === grid[rovers[h].x][rovers[h].y]){ 
+          console.log("You are about to crash into " +rovers[h].name+ ", can we do something else please?")
+          crashE = 1
+          ;break
+          } 
+        }
       }
-      else{ 
-      rover.y += 1;
-      
-      
-      break;}
+        if (crashE === 0) {
+          rover.y += 1;
+          } break; 
   }
   console.log(rover.name + " moveForward was called: current position: " + rover.x + " / " + rover.y);
   rover.travelLog.push("["+rover.x+" , "+rover.y+"]");
+  
 }
 
 function moveBackward(rover){
-  var notCurrentRover;
-  if(rover === rover1){notCurrentRover = rover2;} else {notCurrentRover = rover1;}
+  var crashBN = 0;
+  var crashBS = 0;
+  var crashBW = 0;
+  var crashBE = 0;
   switch (rover.direction) {
     case "N":
       if (rover.x === 9) {
-        console.log("This move is not possible " +rover.name+ " please provide a new instruction.");
+        console.log("You are about to get out of the grid with " +rover.name+ ", please provide a new instruction.");
         break;
       }
       else if(grid[rover.x+1][rover.y] === "X"){ 
-        console.log("There is an obstacle for " +rover.name+ " can we do something else please?")
+        console.log("There is an obstacle for " +rover.name+ ", can we do something else please?");
         break;
       }
-      else if(grid[rover.x+1][rover.y] === grid[notCurrentRover.x][notCurrentRover.y]){ 
-        console.log("You are about to crash into " +notCurrentRover.name+ " can we do something else please?")
-        break;
+      else if(rover.x !== 9){
+        for (h=0; h<rovers.length; h++){ 
+          if(grid[rover.x+1][rover.y] === grid[rovers[h].x][rovers[h].y]){ 
+          console.log("You are about to crash into " +rovers[h].name+ ", can we do something else please?")
+          crashBN = 1
+          ;break
+          } 
+        }
       }
-      else{ 
-      rover.x += 1;
-      break;}
+        if (crashBN === 0) {
+          rover.x += 1;
+          } break;  
 
     case "S":
       if (rover.x === 0) {
-        console.log("You are about to get out of the grid with " +rover.name+ " please provide a new instruction.");
-        break;
-      }
-      else if(grid[rover.x-1][rover.y] === "X"){ 
-        console.log("There is an obstacle for " +rover.name+ " can we do something else please?")
-        break;
-      }
-      else if(grid[rover.x-1][rover.y] === grid[notCurrentRover.x][notCurrentRover.y]){ 
-        console.log("You are about to crash into " +notCurrentRover.name+ " can we do something else please?")
-        break;
-      }
-      else{ 
-      rover.x -= 1;
+      console.log("You are about to get out of the grid with " +rover.name+ ", please provide a new instruction.");
       break;}
+      else if(grid[rover.x-1][rover.y] === "X"){ 
+        console.log("There is an obstacle for " +rover.name+ ", can we do something else please?");
+        break;
+      }
+      else if(rover.x !== 0){
+        for (h=0; h<rovers.length; h++){ 
+          if(grid[rover.x-1][rover.y] === grid[rovers[h].x][rovers[h].y]){ 
+          console.log("You are about to crash into " +rovers[h].name+ ", can we do something else please?")
+          crashBS = 1
+          ;break
+          } 
+        }
+      }
+        if (crashBS === 0) {
+          rover.x -= 1;
+          } break;  
 
     case "W":
       if (rover.y === 9) {
-        console.log("You are about to get out of the grid with " +rover.name+ " please provide a new instruction.");
+        console.log("You are about to get out of the grid with " +rover.name+ ", please provide a new instruction.");
         break;
       }
       else if(grid[rover.x][rover.y+1] === "X"){ 
-        console.log("There is an obstacle for " +rover.name+ " can we do something else please?")
+        console.log("There is an obstacle for " +rover.name+ ", can we do something else please?");
         break;
       }
-      else if(grid[rover.x][rover.y+1] === grid[notCurrentRover.x][notCurrentRover.y]){ 
-        console.log("You are about to crash into " +notCurrentRover.name+ " can we do something else please?")
-        break;
+      else if(rover.y !== 9){
+        for (h=0; h<rovers.length; h++){ 
+          if(grid[rover.x][rover.y+1] === grid[rovers[h].x][rovers[h].y]){ 
+          console.log("You are about to crash into " +rovers[h].name+ ", can we do something else please?")
+          crashBW = 1
+          ;break
+          } 
+        }
       }
-      else{ 
-      rover.y += 1;
-      break;}
+        if (crashBW === 0) {
+          rover.y += 1;
+          } break; 
 
     case "E":
       if (rover.y === 0) {
-        console.log("You are about to get out of the grid with " +rover.name+ " please provide a new instruction.");
+        console.log("You are about to get out of the grid with " +rover.name+ " ,please provide a new instruction.");
         break;
       }
       else if(grid[rover.x][rover.y-1] === "X"){ 
-        console.log("There is an obstacle for " +rover.name+ " can we do something else please?")
+        console.log("There is an obstacle for " +rover.name+ " ,can we do something else please?");
         break;
       }
-      else if(grid[rover.x][rover.y-1] === grid[notCurrentRover.x][notCurrentRover.y]){ 
-        console.log("You are about to crash into " +notCurrentRover.name+ " can we do something else please?")
-        break;
+      else if(rover.y !== 0){
+        for (h=0; h<rovers.length; h++){ 
+          if(grid[rover.x][rover.y-1] === grid[rovers[h].x][rovers[h].y]){ 
+          console.log("You are about to crash into " +rovers[h].name+ ", can we do something else please?")
+          crashBE = 1
+          ;break
+          } 
+        }
       }
-      else{ 
-      rover.y -= 1;
-      break;}
+        if (crashBE === 0) {
+          rover.y -= 1;
+          } break; 
   }
   console.log(rover.name + " moveBackward was called: current position: " + rover.x + " / " + rover.y);
   rover.travelLog.push("["+rover.x+" , "+rover.y+"]");
+  
 }
+
 
 function commands(listOfCommands) {
   //choosing first player - this would be cool with multiple rovers, but I am not sure how to at this point :)
